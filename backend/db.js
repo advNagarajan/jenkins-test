4d8967156fa2b45d4ae1d67dff3b63ae4bd55717
Aadhav Nagarajan <aadhavps220@gmail.com>
2026-02-26 18:15:04 +0530

    i cant do this anymore

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
 
+    /*
 host:process.env.DB_HOST,
 port:process.env.DB_PORT,
 
@@ -11,7 +12,12 @@ user:process.env.DB_USER,
 password:process.env.DB_PASSWORD,
 
 database:process.env.DB_NAME,
-
+*/
+host: "database-1.chmgqe0e2heu.ap-south-1.rds.amazonaws.com",
+  port: 5432,
+  user: "postgres",
+  password: "Aadhav123!",
+  database: "postgres",
 ssl:{
 rejectUnauthorized:false
 }
